e3fdd85b6426d4995bfcddf84fc1b7f27321b4a0
Cory Grant <cory.grant11@gmail.com>
2025-08-27 22:50:07 -0400

    setting up chibi files

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "ch.h"
+#include "hal.h"
+
+#include "canboard_config.h"
+#include "can.h"
+#include "analog.h"
+#include "rotary_switch.h"
+
+/*
+ * Application entry point.
+ */
+int main(void)
+{
+
+  halInit();
+  chSysInit();
+
+  InitCan();
+
+  while (true)
+  {
+    UpdateSwPos();
+    chThdSleepMilliseconds(500);
+  }
+}
